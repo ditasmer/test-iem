cf773d1a0a504c63df003fe263931fa14e5a9ff3
ditasmer <ditasmer@gmail.com>
2020-08-02 22:24:27 +0200

    load content from text file

--- cursos.php
+++ cursos.php
@@ -27,25 +27,12 @@ require('multiidioma/language.php');
 		    </div>
 
 		    <div class="sections"  id="cursos">
-		    	<h1>Idiomas (desde los 4 años).</h1>
-
-				<p>Horarios de mañana, tarde, noche y sábados mañana.</p>
-
-				<p>Grupos homogéneos, agrupados por edades y niveles</p>
-
-				<p>Grupos reducidos.</p>
-
-				<p>Preparación de los exámenes de la Univesidad de Cambridge o de la Escuela Oficial de idiomas.</p>
-
-				<h1>Refuerzo escolar de todas las materias</h1>
-
-				<p>Para todos los niveles escolares</p>
-
-				<p>Trabajamos con los libros que utilizan los alumnos</p>
-
-				<p>Seguimiento personalizado.</p>
-
-				<p>Técnicas de estudio.</p>
+		    	<!--lectura del texto descripcion cursos-->
+		    	<?php
+					if(file_exists('files/id_cursos.txt')){
+						readfile('files/id_cursos.txt');
+					}
+				?>
 
 			</div>
 		</div>
