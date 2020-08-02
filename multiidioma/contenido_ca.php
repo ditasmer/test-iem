0265b8a12e4e150446aafb8b14e58dc739f08d00
ditasmer <ditasmer@gmail.com>
2020-08-02 20:16:05 +0200

    footer, navbar traduction

--- multiidioma/contenido_ca.php
+++ multiidioma/contenido_ca.php
@@ -1,6 +1,12 @@
 <?php
 $titulo1 = 'Institut';
 $titulo2 = 'd´Estudis Moderns';
-
+$menu1 = 'L´ESCOLA';
+$menu2 = 'CURSOS I HORARIS';
+$menu3 = 'SITUACIÓ I CONTACTE';
+$titulo_footer = 'Comparteix a:';
+$txt_face = 'Comparteix a facebook';
+$txt_twi = 'Comparteix a twitter';
+$txt_goo = 'Comparteix a google+';
 
 ?>
--- multiidioma/contenido_es.php
+++ multiidioma/contenido_es.php
@@ -1,6 +1,12 @@
 <?php
 $titulo1 = 'Instituto';
 $titulo2 = 'de Estudios Modernos';
-
+$menu1 = 'LA ESCUELA';
+$menu2 = 'CURSOS Y HORARIOS';
+$menu3 = 'SITUACIÓN Y CONTACTO';
+$titulo_footer = 'Comparte en:';
+$txt_face = 'Comparte en facebook';
+$txt_twi = 'Comparte en twitter';
+$txt_goo = 'Comparte en google+';
 
 ?>
