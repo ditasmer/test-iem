a8affc9d9864e9606a0c92bc5aaa4b4a160a6fbb
ditasmer <ditasmer@gmail.com>
2020-08-03 09:58:10 +0200

    ficheros traduccion form

---
+++ js/vars_js_ca.js
@@ -0,0 +1,7 @@
+//form.js
+let nombre='Nom';
+let email='Email';
+let mensaje='Missatge';
+let avisemail='Si us plau introdueixi una adreça de correu electrónic vàlida';
+let avisprivacitat='Es obligatori acceptar la normativa de privacitat';
+let avisrevisar='Si us plau revisi les següents dades';
---
+++ js/vars_js_es.js
@@ -0,0 +1,7 @@
+//form.js
+let nombre='Nombre';
+let email='Email';
+let mensaje='Mensaje';
+let avisemail='Por favor, introduzca una dirección de correo válida.';
+let avisprivacitat='Es obligatorio aceptar la normativa de privacidad';
+let avisrevisar='Por favor revise los siguientes datos:';
